06c9c84b04583846847534744efaf713ce675b22
yoyofx <zl.hxd@hotmail.com>
2023-05-16 17:34:28 +0800

    init

---
+++ dotnetmetric/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dotnetmetric.Controllers
+{
+
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet("hello")]
+        public string Hello()
+        {
+            return "hello world";
+        }
+    }
+}
+
